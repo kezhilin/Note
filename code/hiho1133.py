b71dd9f42eaa2e071eaa369424d5a4eed6ee75d4
kezhilin <530202947@qq.com>
2017-02-21 16:34:42 +0800

    修正格式

--- code/hiho1133.py
+++ code/hiho1133.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 # 描述
 
-# 在上一回里我们知道Nettle在玩《艦これ》，Nettle的镇守府有很多船位，但船位再多也是有限的。Nettle通过捞船又出了一艘稀有的船，但是已有的N(1≤N≤1,000,000)个船位都已经有船了。所以Nettle不得不把其中一艘船拆掉来让位给新的船。Nettle思考了很久，决定随机选择一个k，然后拆掉稀有度第k小的船。 已知每一艘船都有自己的稀有度，Nettle现在把所有船的稀有度值告诉你，希望你能帮他找出目标船。
+# 在上一回里我们知道Nettle在玩《艦これ》，Nettle的镇守府有很多船位，但船位再多也是有限的。
+# Nettle通过捞船又出了一艘稀有的船，但是已有的N(1≤N≤1,000,000)个船位都已经有船了。
+# 所以Nettle不得不把其中一艘船拆掉来让位给新的船。Nettle思考了很久，决定随机选择一个k，然后拆掉稀有度第k小的船。
+# 已知每一艘船都有自己的稀有度，Nettle现在把所有船的稀有度值告诉你，希望你能帮他找出目标船。
 # 提示：非有序数组的二分查找之二
 
 # 输入
@@ -12,32 +16,36 @@
 
 # 第1行：一个整数t，表示t在数组中是第k小的数，若K不在数组中，输出-1。
 
-def findK (a, low, high, k):
+
+def findk(low, high):
+    'findk'
     first = low
     last = high
-    key = a[first]
+    key = A[first]
     while first < last:
-        while first < last and a[last] >= key:
+        while first < last and A[last] >= key:
             last -= 1
-        a[first] = a[last]
-        while first < last and a[first] <= key:
+        A[first] = A[last]
+        while first < last and A[first] <= key:
             first += 1
-        a[last] = a[first]
-    a[first] = key
-    if first < k:
-        return findK(a, first + 1, high, k)
-    elif first > k:
-        return findK(a, low, first - 1, k)
+        A[last] = A[first]
+    A[first] = key
+    if first < K:
+        return findk(first + 1, high)
+    elif first > K:
+        return findk(low, first - 1)
     else:
         return key
-    
+
+
 while True:
-    try: 
-        (n, k) = (int(x) for x in raw_input().split())
-        a = list((int(x) for x in raw_input().split()))
-        if n < k:
-            print(-1)
+    try:
+        (N, K) = (int(x) for x in raw_input().split())
+        A = list((int(x) for x in raw_input().split()))
+        if N < K:
+            print -1
         else:
-            print(findK(a, 0, len(a) - 1, k - 1))
+            K -= 1
+            print findk(0, len(A) - 1)
     except EOFError:
         break
